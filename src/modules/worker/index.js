8d1ffad93d8486b94039e3359ae6d2b5d35bea91
thientruong <thanhthien5764@gmail.com>
2024-10-19 10:41:47 +0700

    update: remove reule iptables

--- src/modules/worker/index.js
+++ src/modules/worker/index.js
@@ -8,7 +8,7 @@ Route.post('/cdr/downloadCDR', verifyToken, worker.fetchCDRToDownload)
 Route.get('/migrateCDR', verifyToken, worker.migrateCDR)
 Route.get('/iptables/rule', verifyToken, worker.getRuleIptables)
 Route.post('/iptables/blacklist', verifyToken, worker.addBlackList)
-Route.post('/iptables/remove', verifyToken, worker.removeRule)
+Route.delete('/iptables/blacklist', verifyToken, worker.removeRule)
 Route.post('/pbx/restart', verifyToken, worker.restartPBX)
 
 export default Route
